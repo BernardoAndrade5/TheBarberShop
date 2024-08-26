e5b28653828c420046daaa96958f9458358b7a7f
BernardoAndrade5 <73230702+BernardoAndrade5@users.noreply.github.com>
2024-08-27 00:30:59 +0100

    SearchActivity Binding Fix

--- app/src/main/java/com/example/thebarbershop/views/searchActivity/SearchActivity.kt
+++ app/src/main/java/com/example/thebarbershop/views/searchActivity/SearchActivity.kt
@@ -2,10 +2,13 @@ package com.example.thebarbershop.views.searchActivity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.thebarbershop.R
+import com.example.thebarbershop.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySearchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 }}
