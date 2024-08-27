aed5b822f6a6726805d3bc1facddceef8bfaadf4
David Soares <davidsanchescastro1@gmail.com>
2024-08-27 10:57:29 +0100

    Tela de pesquisa

--- app/src/main/java/com/example/thebarbershop/views/searchActivity/SearchViewModel.kt
+++ app/src/main/java/com/example/thebarbershop/views/searchActivity/SearchViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.viewModelScope
 import com.example.thebarbershop.repositorys.AppointmentRepository
 import com.example.thebarbershop.repositorys.AuthRepository
 import com.example.thebarbershop.repositorys.BusinessRepository
-import com.example.thebarbershop.uiStates.HomeUiState
 import com.example.thebarbershop.uiStates.SearchUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -19,42 +18,32 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject
-constructor(
+class SearchViewModel @Inject constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
+
     private val _uiState = MutableStateFlow(SearchUiState())
     val uiState: StateFlow<SearchUiState> = _uiState
 
     init {
-    }
-
-
-
-
-
-    private fun loadUserFlow(): Flow<SearchUiState> = flow {
-        emit(SearchUiState(isLoading = true))
-        try {
-            // Emit initial state with isAuthenticated = false and userEmail = null
-            emit(SearchUiState(isAuthenticated = false, userEmail = null.toString(), isLoading = false))
-
-            val isAuthenticated = authRepository.isUserAuthenticated()
-            val userEmail = authRepository.getCurrentUserEmail().toString()
-            emit(SearchUiState(isAuthenticated = isAuthenticated, userEmail = userEmail, isLoading = false))
-        } catch (e: Exception) {
-            emit(SearchUiState(errorMessage = e.message, isLoading = false))
+        viewModelScope.launch {
+            loadUserFlow().collect { _uiState.value = it }
         }
     }
 
+    private fun loadUserFlow(): Flow<SearchUiState> = flow {
+        emit(SearchUiState(isLoading = true))
+        val isAuthenticated = authRepository.isUserAuthenticated()
+        val userEmail = authRepository.getCurrentUserEmail().toString();
+        emit(SearchUiState(isAuthenticated = isAuthenticated, userEmail = userEmail, isLoading = false))
+    }
 
     fun logoutCurrentUser() {
         authRepository.signOutCurrentUser()
     }
 
-    fun getUserEmail(){
+    fun getUserEmail() {
         authRepository.getCurrentUserEmail()
     }
-
 }
 
